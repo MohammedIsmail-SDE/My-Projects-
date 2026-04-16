da1d4d52a245a03481503187226e20322a4c7719
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-04-16 08:04:08 +0530

    updated fille

---
+++ pratice.py
@@ -0,0 +1,25 @@
+
+#PRATICE-1
+# input_a = int(input("Enter a number: "))
+# input_b = int(input("Enter another number: "))
+# result = input_a + input_b
+
+# print("The sum is:", result )
+
+#PRATICE-2
+# A = float(input("enter the area :"))
+# B = float(input("enter the area :"))
+
+# print ( "The area is :"  ,( A+B )/2  )
+
+#PRATICE-3
+# A  = int(input("Enter number :"))
+
+# B  = int(input("Enter number :"))
+# print (A>=B)
+
+# str = "My name is ismail and from hyderabad and hyderabad is a it hub city"
+# print (str.find("hyderabad"))
+
+name = input("Enter your name :")
+print("Length of my name is :", len(name))
